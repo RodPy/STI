13db8c203020bcbaea70128f80fd79fa691026d0
Paabloinsfran <56365331+Paabloinsfran@users.noreply.github.com>
2019-10-16 18:02:27 -0300

    LSD
    Libreria y codigo

---
+++ Grupo 2. JURUPO/Mensajes_LCD/Mensajes_LCD.ino
@@ -0,0 +1,30 @@
+#include<Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);// 0x3F , 0x20 , 0x38 , 
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("SAMSUNG TECH"); 
+  lcd.setCursor(3,1);
+  lcd.print("INSTITUTE "); 
+  delay(3000); 
+}
+
+void loop()  {
+
+  smsLCD("Buenas", "Tardes");
+
+  }
+
+
+void smsLCD(String a , String b){
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print(a); 
+   lcd.setCursor(1,1);
+  lcd.print(b); 
+  delay(3000); 
+  
+  }
